8905a11f5c66bb614898d0f4f60a096c48da9411
LoRa User <user@example.com>
2025-11-23 08:00:03 +0530

    fixed servers

--- frontend/src/pages/Servers.jsx
+++ frontend/src/pages/Servers.jsx
@@ -821,6 +821,7 @@ export default function Servers() {
                             </div>
                         ))}
                     </div>
+                    )}
                 </div>
             </div>
         );
