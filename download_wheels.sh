b01f38c5e75ada1f2c0fba57c0a3b5ae321e8abf
Rohan Pawar <rohan.pawar23@spit.ac.in>
2025-12-06 18:39:08 +0530

    cleaned unecessary files

--- download_wheels.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir -p backend/wheels
-pip download \
-    --platform manylinux_2_17_aarch64 \
-    --python-version 3.13 \
-    --only-binary=:all: \
-    --dest backend/wheels \
-    -r backend/requirements.txt
